925ddcd3a81afe701c19bfadb1f84f8451f2fcef
Emirhan ETLİ <emirhanetli2003@gmail.com>
2024-01-01 23:00:23 +0300

    added listing flights

--- AircraftReservationSystem/Areas/User/Controllers/HomeController.cs
+++ AircraftReservationSystem/Areas/User/Controllers/HomeController.cs
@@ -57,5 +57,11 @@ namespace AircraftReservationSystem.Areas.User.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult GoTicket(FlightInformation flight)
+        {
+            return RedirectToAction("Ticket");
+
+        }
     }
 }
